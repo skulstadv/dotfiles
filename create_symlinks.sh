cee3a2c24ccb9b8dcdf2b6a3377aacc90fa493c0
blla <skulstadv@gmail.com>
2018-08-12 12:43:42 +0200

    Add script for creating symlinks

---
+++ create_symlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "symlink for .inputrc"
+ln -s ~/.dotfiles/.inputrc ~/.inputrc
+echo "symlink for .vimrc"
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+echo "symlink for .tmux.conf"
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
