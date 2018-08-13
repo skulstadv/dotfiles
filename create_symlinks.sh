90333b6bab4177f46cf9634f14ca4381647bc212
blla <skulstadv@gmail.com>
2018-08-13 17:16:09 +0200

    Added .bashrc for autocompletion

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -5,3 +5,5 @@ echo "symlink for .vimrc"
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 echo "symlink for .tmux.conf"
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+echo "symlink for .tmux.conf"
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
